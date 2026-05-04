fe6558a1230bc73369be657a5c8a68ac62748f0f
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2026-05-04 03:23:52 -0700

    implement the about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Coktails from "./components/Coktails";
+import About from "./components/About";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <Coktails />
+        <About />
     </main>
   )
 }
