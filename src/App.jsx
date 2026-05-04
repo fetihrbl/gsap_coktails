4af330c0bf8c8b1817f3826e815bc14f0eca5c20
Fatih Erbil <fatiherbil@Fatihs-MacBook-Air.local>
2026-05-04 02:09:03 -0700

    Implement Cocktails Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Coktails from "./components/Coktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,7 +12,7 @@ const App = () => {
     <main>
         <Navbar />
         <Hero />
-        
+        <Coktails />
     </main>
   )
 }
